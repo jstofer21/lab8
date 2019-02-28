0f2d823341b45ac0d42fe1d473f4075268aaebc6
jon <stoferjon@gmail.com>
2019-02-28 11:13:32 -0800

    lab finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,18 +10,48 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
-// init jQuery gestures  
+// init jQuery gestures
 function initGestures() {
 	// add gestures listener here
+  $(function(){
+    $(".judge-img").bind("taphold",tapholdHandler);
+
+    function tapholdHandler(event){
+      //get rid of id of event source
+      var targetIDPrefix = event.target.id;
+      console.log("got prefix:"+targetIDPrefix);
+      //show bio
+      $("#"+targetIDPrefix+"-bio").show();
+    }
+  });
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e){
+    e.preventDefault();
+    console.log("submitting form");
+    var rsvpEmail = $('#rsvpEmail').val();
+    $.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+  });
+
+  function postCallback(res) {
+    alert("RSVP email successfully submitted");
+    $('#rsvpEmail').val('')//clear form
+  }
 }
+
+/**
+$(function(){
+  $("div.box").bind("taphold", tapholdHandler);
+  function tapholdHandler(event){
+    $(event.target).addClass("taphold");
+  }
+});**/
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = 'ZEPREXUQHTiOT4S0aSaFuCdKl9i6eASC';
+
+  var map = L.mapquest.map('map', {
+    center: [32.88,-117.236],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+  //$("#map") = map;
+  L.marker([32.88,-117.24]).addTo(map);
+
 }
